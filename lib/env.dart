f55c94a80c6416a64facd47bfd5ced7a1916a62f
JoE11-y <Josephedoh77@gmail.com>
2025-01-27 12:14:25 +0100

    add starknet updates

--- lib/env.dart
+++ lib/env.dart
@@ -4,4 +4,5 @@ const environment = {
   'wsUrl': 'wss://staging.api.themarquis.xyz/ws',
   'wsUrlDebug': 'wss://staging.api.themarquis.xyz/ws',
   'build': 'DEBUG',
+  'rpcUrl': 'https://starknet-sepolia.infura.io/v3/2E9u9FUJucyGntaYzan2LNJdEGK'
 };
